78d99d0d227eea47e7b7ecd2396b3ef7f60fbae6
Eduardo04-32 <jesus.sanchez32.12@gmail.com>
2026-02-20 14:11:34 -0300

    agora ta com o erro que deve ser corrigido

---
+++ ex2.js
@@ -0,0 +1,14 @@
+
+function VerificarChuva(provavilidade){  
+
+    if ( provavilidade >= 10,0 ){
+        alert("Leve um guarda-chuvas!")
+    }else{
+        alert("Pode andara de boa na rua!")
+    }
+
+    
+}
+let provavilidade = Number(prompt("Qual é a provavilidades em porcentagem que há para chver? leve? forte? "))
+
+VerificarChuva(provavilidade)
