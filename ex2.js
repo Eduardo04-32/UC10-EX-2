7e3335dec2bc28038d8b63d844950d65d3e71424
LuizVCM <luizvcmluizvcm@gmail.com>
2026-02-20 14:23:12 -0300

    Código atualizado e verificado. Por favo, observar atentamente o  processo realizado

--- ex2.js
+++ ex2.js
@@ -1,7 +1,7 @@
 
 function VerificarChuva(provavilidade){  
 
-    if ( provavilidade >= 10,0 ){
+    if ( provavilidade >= 10.0 ){
         alert("Leve um guarda-chuvas!")
     }else{
         alert("Pode andara de boa na rua!")
@@ -12,3 +12,5 @@ function VerificarChuva(provavilidade){
 let provavilidade = Number(prompt("Qual é a provavilidades em porcentagem que há para chver? leve? forte? "))
 
 VerificarChuva(provavilidade)
+
+// Só funciona de rodar no navegador
